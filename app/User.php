efe9f79629c39363930dd5705544cba3cf3e76c2
Aleem Isiaka <aleemisiaka@gmail.com>
2020-07-19 22:08:29 +0100

    Implements user authentication

--- app/User.php
+++ app/User.php
@@ -3,8 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Contracts\Auth\Authenticatable;
 
-class User extends Model
+class User extends Model implements Authenticatable
 {
     //
+    protected $fillable = ['name', 'email', 'password']; 
+    protected $guarded = ['password']; 
 }
