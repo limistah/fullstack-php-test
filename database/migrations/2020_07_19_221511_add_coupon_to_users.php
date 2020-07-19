e1760d9a40f1dc9b8b8b7cda8183226b87400898
Aleem Isiaka <aleemisiaka@gmail.com>
2020-07-19 23:17:56 +0100

    Added migrations to include Tax fee and coupons to users

--- database/migrations/2020_07_19_221511_add_coupon_to_users.php
+++ database/migrations/2020_07_19_221511_add_coupon_to_users.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddCouponToProducts extends Migration
+class AddCouponToUsers extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class AddCouponToProducts extends Migration
      */
     public function up()
     {
-        Schema::table('products', function (Blueprint $table) {
+        Schema::table('users', function (Blueprint $table) {
             $table->bigInteger('coupon_id');
         });
     }
@@ -25,6 +25,5 @@ class AddCouponToProducts extends Migration
      */
     public function down()
     {
-        
     }
 }
--- database/migrations/2020_07_19_221551_add_tax_fee_to_users.php
+++ database/migrations/2020_07_19_221551_add_tax_fee_to_users.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddCouponToProducts extends Migration
+class AddTaxFeeToUsers extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,8 @@ class AddCouponToProducts extends Migration
      */
     public function up()
     {
-        Schema::table('products', function (Blueprint $table) {
-            $table->bigInteger('coupon_id');
+        Schema::table('users', function (Blueprint $table) {
+            $table->integer('tax_fee');
         });
     }
 
@@ -25,6 +25,5 @@ class AddCouponToProducts extends Migration
      */
     public function down()
     {
-        
     }
 }
