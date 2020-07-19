f313cdef69f7a1faed7237a4d90a2d4aed16c7de
Aleem Isiaka <aleemisiaka@gmail.com>
2020-07-19 22:55:50 +0100

    Product seeder

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\Product;
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
+
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| This directory should contain each of the model factory definitions for
+| your application. Factories provide a convenient way to generate new
+| model instances for testing / seeding your application's database.
+|
+*/
+
+$factory->define(Product::class, function (Faker $faker) {
+    $filnames = [
+        url('/storage/products/bag.jpeg'),
+        url('/storage/products/bag_2.jpeg'),
+        url('/storage/products/bag_3.jpeg')
+    ];
+    return [
+        'title' => $faker->name,
+        'price' => $faker->unique()->randomNumber($nbDigits = 8),
+        'description' => $faker->text(100),
+        'image' => $faker->randomElement($filnames),
+        'published' => $faker->randomElement([true, false])
+    ];
+});
